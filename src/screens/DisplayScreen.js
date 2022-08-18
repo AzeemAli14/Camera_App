88d42486adb7a8f9fff399d98c4abf8bc644f940
AzeemAli96 <AzeemAli96@github.com>
2022-08-18 20:59:04 +0500

    Deleted Button added

--- src/screens/DisplayScreen.js
+++ src/screens/DisplayScreen.js
@@ -8,70 +8,87 @@ import {
   Image,
   StatusBar,
   Dimensions,
+  LayoutAnimation,
 } from 'react-native';
 import ImagePicker from 'react-native-image-crop-picker';
 
-const DisplayScreen = ({navigation}) => {
+const DisplayScreen = () => {
   const [fileList, setFileList] = useState([]);
-  const state = useMemo(() => ({fileList}), [fileList]);
-  const [seletedItem, setSelectedItem] = useState([])
+  const [seletedItem, setSelectedItem] = useState([]);
+  const [list, setList] = useState([false])
+  const [count, setCount] = useState(1)
 
-  const onSelectedImage = useCallback(
-    image => {
-      setFileList(fileList => {
-        const newDataImg = [...fileList];
-        const source = {uri: image.path};
-        const isSelected = false;
-        const item = {
-          id: Date.now(),
-          url: source,
-          content: image.data,
-        };
-        newDataImg.push(item);
-        return newDataImg;
-      });
-    },
-    [setFileList],
-  );
+  const increment =useCallback( () => {
+    console.log("Increment...",count)
+    let counter = count +1;
+    setCount(counter)
+    console.log("Increment...",counter)
+  },[count])
+
+
+  const onSelectedImage = useCallback(image => {
+    setFileList(fileList => {
+      let newDataImg = [...fileList];
+
+      const source = {uri: image.path};
+      // const listState = this.state;
+      let item = {
+        id: Math.floor(Math.random(1)*100),
+        url: source,
+        content: image.data,
+      };
+      newDataImg.push(item);
+      return newDataImg;
+    });
+  }, [onSelectedImage]);
 
   const takePhotoFromCamera = useCallback(() => {
     ImagePicker.openCamera({
       width: 300,
       height: 400,
     }).then(image => {
+      // console.log(image, 'image');
       onSelectedImage(image);
-      console.log(image);
+      increment()
+
     });
-  }, [onSelectedImage]);
+  }, []);
 
-  const getSelected = (item) => {
-    seletedItem.includes(item.id)
-  }
-
-  useCallback(() => {
-    const handleLongPress = (item, index) => {
-      // setSelectedItem([seletedItem, item.id])
-      const newArr = fileList.map((e, index) => {
-        if(item.id == e.id) {
-          return {
-            ...e,
-            selected : true
-          }
-        }
-  
-        return {
-          selected : false
-        }
-      })
-  
-      setdata(newArr);
-    }
-  
+  useEffect(() => {
+    setList(list, true)
+    return () => {
     
-  }, [])
+    }
+  }, [fileList])
   
 
-  
+  // const getSelected = (item) => {
+  //   seletedItem.includes(item.id)
+  // }
+
+  // let listState = fileList;
+  // const onClickItem = (item, index) => {
+  //   // setSelectedItem([seletedItem, item.id])
+  //   // alert(item.id)
+  //   const newArr = fileList.map((e, index) => {
+  //     if (item.id == e.id) {
+  //       return {
+  //         ...e,
+  //         selected: true,
+  //       };
+  //     }
+
+  //     return {
+  //       selected: false,
+  //     };
+  //   });
+
+  //   // setdata(newArr);
+  // };
+
+  // useCallback(() => {
+
+  // }, [])
 
   // alert(seletedItem)
 
@@ -92,25 +109,65 @@ const DisplayScreen = ({navigation}) => {
       });
   }, [onSelectedImage]);
 
-  const renderItem = useCallback(({item, index, selected}) => {
+  const onDelete = id => {
+
+    console.log("id is ", fileList)
+
+    // console.log('On Delete....fileList', index);
+  //   const arr = fileList.filter(function(item) {
+  //     return item.id !== id
+  //   })
+  //   // let filterData = fileList.filter(item => item.index !== index);
+  //   // console.log('Filtrer Data.....................', filterData);
+  //   setFileList(arr);
+  //   LayoutAnimation.configureNext(layoutAnim)
+  };
+
+  const layoutAnim = {
+    duration: 300,
+    update: {
+      type: LayoutAnimation.Types.easeInEaseOut, 
+    },
+    delete: {
+      duration: 100,
+      type: LayoutAnimation.Types.easeInEaseOut,
+      property: LayoutAnimation.Properties.opacity,
+    },
+  };
+
+  const renderItem = useCallback(({item, index, selected, id}) => {
     return (
       <View style={styles.flatlist}>
         <TouchableOpacity
-        onPress={() => alert(index, "Seleted")}
-        onLongPress={() => handleLongPress(item, index)}
-        style={[styles.item, {
-          
-          backgroundColor: item.selected ? styles.overlay : "white"
-        }]}
-        >
+          onPress={() => onClickItem(item, index)}
+          // onLongPress={() => handleLongPress(item, index)}
+          style={[
+            styles.item,
+            {
+              backgroundColor: item.selected ? styles.overlay : 'white',
+            },
+          ]}>
           <View style={{padding: 8}}>
-          <Image source={item.url} style={styles.itemImage} />
+            <Image source={item.url} style={styles.itemImage} />
+            <View
+              style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+              <TouchableOpacity
+                style={styles.buttonStyle}
+                activeOpacity={1.5}
+                onPress={() => onDelete(fileList.id)}>
+                <Image
+                  source={require('../images/delete.png')}
+                  style={styles.imageStyle}
+                />
+                <Text style={styles.textStyle}>Delete</Text>
+              </TouchableOpacity>
+            </View>
           </View>
-          {selected && <View style={styles.overlay}/>}
+          {/* {selected && <View style={styles.overlay}/>} */}
         </TouchableOpacity>
       </View>
     );
-  }, []);
+  }, [fileList]);
 
   return (
     <View style={styles.container}>
@@ -120,22 +177,22 @@ const DisplayScreen = ({navigation}) => {
         data={fileList}
         keyExtractor={(item, index) => index.toString()}
         renderItem={renderItem}
-        extraData={state}
+        // extraData={state}
         columnWrapperStyle={{
           justifyContent: 'space-between',
-          margin: 15
+          margin: 15,
         }}
-        selected = {getSelected(renderItem)}
+        // selected = {getSelected(renderItem)}
       />
 
       <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-        <TouchableOpacity style={styles.viewData} onPress={takePhotoFromCamera}>
+        <TouchableOpacity style={styles.viewData} onPress={(list) => takePhotoFromCamera(setFileList)}>
           <Text>Camera</Text>
         </TouchableOpacity>
 
         <TouchableOpacity
           style={styles.viewData}
-          onPress={choosePhotoFromLibrary}>
+          onPress={() => choosePhotoFromLibrary()}>
           <Text>Gallery</Text>
         </TouchableOpacity>
       </View>
@@ -158,11 +215,11 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   flatlist: {
-    backgroundColor: "#D32345",
-    width: width/2-10,
+    backgroundColor: '#D32345',
+    width: width / 2 - 10,
     // padding: 8,
     borderRadius: 10,
-    overflow: "hidden"
+    overflow: 'hidden',
   },
 
   container2: {
@@ -233,5 +290,19 @@ const styles = StyleSheet.create({
     backgroundColor: 'rbga(0,0,0,0.4)',
     top: 0,
     left: 0,
-  }
+  },
+  imageStyle: {
+    padding: 10,
+    margin: 5,
+    height: 25,
+    width: 25,
+    resizeMode: 'stretch',
+  },
+  textStyle: {
+    color: 'black',
+  },
+  buttonStyle: {
+    width: '100%',
+    height: '100%',
+  },
 });
